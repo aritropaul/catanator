d7d96a09602308f729c1e79c949e2a3f406ff580
Aritro Paul <aritropaul@Aritros-Laptop.local>
2024-05-12 23:09:30 -0400

    new dice logic

--- app/dice/dice.ts
+++ app/dice/dice.ts
@@ -1,16 +1,28 @@
-function randn_bm() {
-    let u = 0, v = 0;
-    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random();
-    let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-    num = num / 10.0 + 0.5; // Translate to 0 -> 1
-    if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
-    return num
+const numbers = [2,3,4,5,6,7,8,9,10,11,12]
+const probs = [1/36, 1/18, 1/12, 1/9, 5/36, 1/6, 5/36, 1/9, 1/12, 1/18, 1/36]
+const totalItems = 150
+
+function generatePool() {
+    let pool: number[] = []
+    for (let i=0;i<11;i++) {
+        let numberOfItems = Math.ceil(totalItems*probs[i])
+        for (let j=0;j<numberOfItems;j++) {
+            pool.push(numbers[i])
+        }
+    }
+    return pool
 }
 
-function diceRoll() {
-    let randomRoll = Math.round(randn_bm()*7)
-    return randomRoll
+function diceRoll(pool: number[]) {
+    let randomIndex = Math.floor(Math.random() * pool.length)
+    let randomRoll = pool[randomIndex];
+    pool.splice(randomIndex, 1);
+    return {randomRoll , pool}
 }
 
-export default diceRoll
+function dieRolls(times: number) {
+    generatePool()
+    // console.log()
+}
+
+export  { generatePool, diceRoll, dieRolls }
--- app/dice/page.tsx
+++ app/dice/page.tsx
@@ -3,25 +3,32 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { useState } from 'react';
-import diceRoll from './dice';
+import { diceRoll, dieRolls, generatePool } from './dice';
+import { BarChart } from '@tremor/react';
 
 export default function Page(){
 
-  const [rolls, setRolls] = useState([3,4]);
+  const [rolls, setRolls] = useState(7);
+  const [data, setData] = useState([ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
+  const [pools, setPools] = useState(generatePool())
 
   function handleRoll() {
-    let d1 = diceRoll()
-    let d2 = diceRoll()
-    setRolls([d1,d2])
+      let roll = diceRoll(pools)
+      console.log(roll)
+      setRolls(roll.randomRoll)
+      setPools(roll.pool)
+      if (pools.length < 10) {
+        setPools(generatePool())
+      }
   }
 
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-between p-12 bg-neutral-900">
-      <div className="w-fit lg:w-[600px] items-center justify-between">
-        <div className='md:inline-block items-center justify-between w-full text-white font-normal md:px-0 px-4'>
+      <div className="w-fit lg:w-[600px] mt-[20%] items-center justify-between">
+        <div className='md:inline-block items-center justify-between w-full text-neutral-200 font-normal md:px-0 px-4'>
           <div className='w-full text-sm font-normal text-neutral-200 items-center justify-between text-center'>You rolled</div>
-          <div className='text-[72px] items-center justify-between w-full text-white text-center font-normal md:px-0 px-4'>
-            { rolls[0] } , { rolls[1] }
+          <div className='text-[72px] items-center justify-between w-full text-neutral-200 text-center font-normal md:px-0 px-4'>
+            {/* { rolls[0] } , { rolls[1] } */ rolls}
           </div>
           <Button className='dark w-full bg-neutral-900 text-neutral-200 hover:bg-neutral-900 border-none px-0 font-normal' onClick={handleRoll} variant="link">
             next roll
